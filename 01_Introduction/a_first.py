db76c5da8be7bb616bc812e00ecf659ffe53082e
rdodda99 <160366956+rdodda99@users.noreply.github.com>
2024-12-16 17:48:19 +0000

    upadated file sin the repo

---
+++ 01_Introduction/a_first.py
@@ -0,0 +1,20 @@
+x = 10
+print(x)
+
+y = 20
+z = x+y
+print(y)
+print(z)
+
+print(z)
+print("z")
+
+print(z,"z")
+print("z",z)
+
+print("z = ",z)
+
+# single line comments
+"""Multi
+line 
+commands"""
